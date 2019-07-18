2ef7b0b8358d6c51c4e249d6e4e3c669c3b3a72c
Zaanetti <40700571+Zaanetti@users.noreply.github.com>
2019-07-17 22:04:44 -0300

    Add files via upload

--- facens.py
+++ facens.py
@@ -75,3 +75,91 @@ f = (9 * (c / 5) + 32)
 print(f'Temperatura em ºF: {f:.2f}')
 """
 
+
+#exercicio lista
+"""
+v = []
+for i in range(5):
+	v.append(int(input('Digite um numero: ')))
+print(v) 
+v2 = []
+[v2.append(int(input('digite: '))) for i in range(5)]
+print(v2)
+
+
+v = [5, 3, 7, 8, 10, 12]
+
+print(max(v))
+
+maxv = 0
+for i in v:
+	if maxv < i:
+		maxv = i
+print(maxv)
+	
+print(v[:4])
+print(v[4:])
+
+
+print(v)
+for i in range(4):
+	print(v[i])
+print('-' * 20)
+for i in range(4, 6):
+	print(v[i])
+
+ """
+# m = [[0,0,0],[0,0,0],[0,0,0]]
+
+# inp = input('O que deseja inserir? ')
+# lin = int(input('Em qual linha? '))
+# col = int(input('Em qual coluna? '))
+# m[lin][col] = inp
+
+# [print(l) for l in m
+
+
+# v = []
+# [[v.append(0) for l in range(3)] for c in range(5)]
+# [print(l) for l in v]
+def imprimiMatriz():
+	[print(l) for l in lugares]
+
+
+def contador():
+	cont = 0
+	for lin in range(4):
+		for col in range(5):
+			if lugares[lin][col] != 0:
+				cont += 1
+
+	return print(f'Lugares disponiveis: {20 - cont}')
+
+
+def ocuparCadeiras():
+	asc = int(input('Quantos ascentos irá comprar? '))
+	for i in range(asc):
+		while asc > 0:
+			f_sel = int(input('Fileira desejada: '))
+			c_sel = int(input('Cadeira desejada: '))
+			if lugares[f_sel][c_sel] == 0:
+				lugares[f_sel][c_sel] = 1
+				asc -= 1
+			else:
+				print('Cadeira já ocupada, delecione outra cadeira.')
+
+
+lugares = []
+cadeiras = []
+
+for i in range(5):
+	cadeiras.append(0)
+for i in range (4):
+	lugares.append(cadeiras.copy())
+while(True):
+	imprimiMatriz()
+	contador()
+	ocuparCadeiras()
+	imprimiMatriz()
+	if str(input('Deseja comprar mais?(S/N) ')).upper == 'N':
+		break
