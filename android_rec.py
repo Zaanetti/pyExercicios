d3c5e761ce3477544fd95716514660f6e37c0e09
Luiz Zanetti <luizzanetti@hotmail.com.br>
2019-08-09 17:47:00 -0300

    android_rec.py: Novos comandos

--- android_rec.py
+++ android_rec.py
@@ -1,21 +1,42 @@
 import os
 import tempfile
 from subprocess import Popen
-
+import getpass
 
 def instalações():
     if input('Instalar android-tools e usbutils(S/N): ').upper() == 'S':
         os.system('sudo yum install android-tools')  # sudo yum install android-tools
         os.system('sudo yum install usbutils')  # sudo yum install usbutils
 
-def con_output(cmd):
+
+def con_output(cmd,tipo='t'):
     with tempfile.TemporaryFile() as tempf:
         proc = Popen([cmd], stdout=tempf)
         proc.wait()
         tempf.seek(0)
-        output = str(tempf.read())[2:-3]
-    return output.split('\\n')
+        if tipo == 't':
+            output = str(tempf.read())[2:-3]
+            return tuple(output.split('\\n'))
+        if tipo == 'str':
+            return str(tempf.read())[2:-3]
 
+
+def devd(device):
+    if 'Bus' in device:
+        ldev = device.split(' ')
+        det = {'bus' : ldev[ldev.index('Bus') + 1],
+               'device' : ldev[ldev.index('Device') + 1],
+               'idfab' : ldev[ldev.index('ID') + 1][:4],
+               'iddev': ldev[ldev.index('ID') + 1][-4:]
+              }
+        return det
+    else:
+        print('Nenhum dispositivo encontrado')
+        return None
+
+
+os.system("exec su -l $luiz")
+os.system("gnome-terminal")
 
 instalações()
 lusb1 = con_output('lsusb')
@@ -28,13 +49,22 @@ lusb2 = con_output('lsusb')
 susb1 = set(lusb1)
 susb2 = set(lusb2)
 
-device = str(lusb2.difference(susb1))[2:-2].strip().lstrip()
+device = str(susb2.difference(susb1))[2:-2].strip().lstrip()  # gera um set de elementos só tem no set2
+print(device)
+det = devd(device)
+print(det)
+if type(det) == dict:
+    # cmd = f"sudo ls -l /dev/bus/usb/{det['bus']}/{det['device']}"
+    # per = con_output(cmd, tipo='str') deu merda ainda não descobri o pq
+    # if 'crw-rw-r--' in per:
+    os.system('sudo groupadd androiddev')
+    os.system(f'sudo usermod - aG androiddev {getpass.getuser()}')
 
-input()
 
-# device_usb = set(lusb2).dif(set(lusb1))  # gera um set de elementos só tem no set2
 
-# print(device_usb)
+# input('FIM')
+
+
 # identificar quem é o aparelho no meio do retorno tratado
 
 # ls -l /dev/bus
