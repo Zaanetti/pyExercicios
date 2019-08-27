bb9b83aecc06b8f0e60084e6ea5af94e7baaf3ec
luiz <luizzanetti@hotmail.com>
2019-08-27 18:06:01 -0300

    1ª etapa finalizada

--- android_rec.py
+++ android_rec.py
@@ -5,11 +5,47 @@ import getpass
 import stat
 
 
+class rules:
+
+    existe = False
+    conteudo = ''
+
+    def check(self):
+        if os.path.exists('/etc/udev/rules.d/99-android-debug.rules'):
+            self.existe = True
+            dir_ant = os.getcwd()
+            os.chdir('/etc/udev/rules.d')
+            with open('99-android-debug.rules', 'r') as arq:
+                self.conteudo = arq.read()
+            os.chdir(dir_ant)
+
+    def delete(self):
+        dir_ant = os.getcwd()
+        os.chdir('/etc/udev/rules.d')
+        os.system('sudo rm -rf 99-android-debug.rules')
+        os.chdir(dir_ant)
+        self.existe = False
+        self.conteudo = ''
+
+    def insert(self, ndevice):
+        os.system('sudo groupadd androiddev')
+        os.system(f'sudo usermod -aG androiddev {getpass.getuser()}')
+        with open('99-android-debug.rules', 'a') as arq:
+            arq.write(ndevice)
+
+
+
+
+
 def instalações():
     if input('Instalar android-tools e usbutils(S/N): ').upper() == 'S':
         os.system('sudo apt-get update')
         os.system('sudo apt install android-tools-adb android-tools-fastboot')
         os.system('sudo apt install usbutils')
+
+        print('=-=-=-=-=-=-= ATUALIZAÇÕES CONCLUÍDAS =-=-=-=-=-=-=')
+        input()
+        os.system('clear')
 
         # os.system('sudo apt install yum')  # sudo apt install yum
         # os.system('sudo yum repolist all')  # sudo yum repolist all
@@ -43,20 +79,19 @@ def devd(device):
         return None
 
 
-# os.mkdir('/etc/udev/rules.d/teste')
-# call(['chmod', '0444', '/home/usuario/Documentos/meus/pyExercicios/android_rec.py'])
-# os.chmod('/home/usuario/Documentos/meus/pyExercicios/android_rec.py', stat.S_IRUSR | stat.S_IRGRP | stat.S_IROTH | stat.S_IWUSR | stat.S_IWGRP | stat.S_IWOTH)
-
 os.system('clear')
-
-ini = os.getcwd()
-
-if os.path.exists('/etc/udev/rules.d/99-android-debug.rules'):
-    if input('Deseja aparagar o 99-android-debug.rules.rules?(s/n)').upper == 'S':
-       reset = True
-
-
 instalações()
+
+crules = rules()
+crules.check()
+
+if crules.existe:
+    if input('Deseja apagar o 99-android-debug.rules(s/n):').upper() == 'S':
+        crules.delete()
+
+print('Caso o aparelho esteja conectado ao computador desconecte ele')
+input()
+
 lusb1 = con_output('lsusb')
 
 print('Habilite a depuração de USB e conecte o cabo USB a um android')
@@ -68,22 +103,26 @@ susb1 = set(lusb1)
 susb2 = set(lusb2)
 
 device = str(susb2.difference(susb1))[2:-2].strip().lstrip()  # gera um set de elementos só tem no set2
-print(device)
+
+if len(device) > 50:
+    print(device)
+
 det = devd(device)
-print(det)
+# print(det)
 if type(det) == dict:
     # cmd = f"sudo ls -l /dev/bus/usb/{det['bus']}/{det['device']}"
-    # per = con_output(cmd, tipo='str') deu merda ainda não descobri o pq
+    # per = con_output(cmd, tipo='str')  # deu merda ainda não descobri o pq
     # if 'crw-rw-r--' in per:
-    rules = f'SUBSYSTEM=="usb", ATTR{{idVendor}}=="{det["idfab"]}", ATTR{{idProduct}}=="{det["iddev"]}", GROUP="androiddev", MODE="0664"'
-    os.system('sudo groupadd androiddev')
-    os.system(f'sudo usermod -aG androiddev {getpass.getuser()}')
-    os.chdir('/etc/udev/rules.d')
-    if reset:
-        os.system('sudo rm -rf 99-android-debug.rules')
-        os.system('sudo touch 99-android-debug.rules')
-    with open('99-android-debug.rules', 'a') as arq:
-        arq.write(rules)
+    nrules = f'SUBSYSTEM=="usb", ATTR{{idVendor}}=="{det["idfab"]}", ATTR{{idProduct}}=="{det["iddev"]}", GROUP="androiddev", MODE="0664"\n'
+    if crules.existe and (nrules in crules.conteudo):
+            print('Dispositivo ja registrado')
+    else:
+        crules.insert(nrules)
+    input('1ª PARTE DO PROCESSO FINALIZADO.\nCaso o terminal não feche automaticamente, feche e execute novamente '
+      'o mesmo arquivo(android_rec.py)')
+else:
+    input()
 
-input('FIM')
-os.system(f'exec su -l ${getpass.getuser()}')
+
+os.system('exit')
+
